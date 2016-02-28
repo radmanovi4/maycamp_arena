aad039224a18d715314ff0521f619270338c883f
Alexander Milchev <amilchev@asteasolutions.com>
2016-02-28 18:53:16 +0200

    Request user_location Facebook permission

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -236,7 +236,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "1560274354293123", "7de82c9f1577ca57b9500a534d7494bc", scope: 'email', info_fields: 'email, name'
+  config.omniauth :facebook, "1560274354293123", "7de82c9f1577ca57b9500a534d7494bc", scope: 'user_location', info_fields: 'email, name, location'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
